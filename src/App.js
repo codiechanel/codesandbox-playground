a5a72e47bea7bd58ccb8a856013b7eecb2e3e19f
codie <codiechanel@gmail.com>
2017-07-25 03:20:49 +0800

    flex

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css'
 import RaisedButton from 'material-ui/RaisedButton'
 import Drawer from 'material-ui/Drawer'
 import AppBar from 'material-ui/AppBar'
@@ -14,6 +14,130 @@ import Home from './Home'
 const homeScreen = ({ location }) =>
   <div>
     <Home mainStore={mainStore} />
+  </div>
+
+// center all child
+const demoScreen1 = () =>
+  <div
+    style={{
+      display: 'flex',
+      flex: 0.75,
+      flexDirection: 'column',
+      backgroundColor: 'gray',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}
+  >
+    <RaisedButton label="demoScreen1" primary />
+
+    <RaisedButton label="demoScreen1" primary />
+  </div>
+
+// equal spacing between elements
+const demoScreen2 = () =>
+  <div
+    style={{
+      display: 'flex',
+      flex: 0.75,
+      padding: 15,
+      flexDirection: 'row',
+      backgroundColor: 'gray',
+      justifyContent: 'space-around',
+      alignItems: 'flex-start'
+    }}
+  >
+    <RaisedButton label="demoScreen2" primary />
+
+    <RaisedButton label="demoScreen2" primary />
+  </div>
+
+// this makes elements apart
+const demoScreen2a = () =>
+  <div
+    style={{
+      display: 'flex',
+      flex: 0.75,
+      padding: 15,
+      flexDirection: 'row',
+      backgroundColor: 'gray',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start'
+    }}
+  >
+    <RaisedButton label="demoScreen2" primary />
+
+    <RaisedButton label="demoScreen2" primary />
+  </div>
+
+// the same as above, but now they start from the bottom
+const demoScreen3 = () =>
+  <div
+    style={{
+      display: 'flex',
+      flex: 0.75,
+      padding: 15,
+      flexDirection: 'row',
+      backgroundColor: 'gray',
+      justifyContent: 'space-around',
+      alignItems: 'flex-end'
+    }}
+  >
+    <RaisedButton label="demoScreen3" primary />
+
+    <RaisedButton label="demoScreen3" primary />
+  </div>
+
+// the same as above, but now they are at the middle
+const demoScreen3a = () =>
+  <div
+    style={{
+      display: 'flex',
+      flex: 0.75,
+      padding: 15,
+      flexDirection: 'row',
+      backgroundColor: 'gray',
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    }}
+  >
+    <RaisedButton label="demoScreen3a" primary />
+
+    <RaisedButton label="demoScreen3a" primary />
+  </div>
+
+// here we align all items to the right, from top to bottom
+const demoScreen4 = () =>
+  <div
+    style={{
+      display: 'flex',
+      flex: 0.75,
+      padding: 15,
+      flexDirection: 'column',
+      backgroundColor: 'gray',
+      justifyContent: 'flex-start',
+      alignItems: 'flex-end'
+    }}
+  >
+    <RaisedButton label="demoScreen4" primary />
+
+    <RaisedButton label="demoScreen4" default />
+  </div>
+
+const demoScreen5 = () =>
+  <div
+    style={{
+      display: 'flex',
+      flex: 0.75,
+      padding: 15,
+      flexDirection: 'row',
+      backgroundColor: 'gray',
+      justifyContent: 'flex-end',
+      alignItems: 'flex-end'
+    }}
+  >
+    <RaisedButton label="demoScreen5" primary />
+
+    <RaisedButton label="demoScreen5" default />
   </div>
 
 const About = () =>
@@ -48,7 +172,15 @@ class App extends React.Component {
     return (
       <Router>
         <MuiThemeProvider>
-          <div className="App">
+          <div
+            style={{
+              display: 'flex',
+              flex: 1,
+              padding: 20,
+              flexDirection: 'column',
+              backgroundColor: 'blue'
+            }}
+          >
             <AppBar
               title="Playground"
               onLeftIconButtonTouchTap={this.handler}
@@ -69,6 +201,41 @@ class App extends React.Component {
                     </Link>
                   </li>
                   <li>
+                    <Link onClick={this.handleClose} to="/demo1">
+                      Demo1
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.handleClose} to="/demo2">
+                      Demo2
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.handleClose} to="/demo2a">
+                      Demo2a
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.handleClose} to="/demo3">
+                      Demo3
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.handleClose} to="/demo3a">
+                      Demo3a
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.handleClose} to="/demo4">
+                      Demo4
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={this.handleClose} to="/demo5">
+                      Demo5
+                    </Link>
+                  </li>
+                  <li>
                     <Link onClick={this.handleClose} to="/about">
                       About
                     </Link>
@@ -78,6 +245,13 @@ class App extends React.Component {
             </Drawer>
 
             <Route exact path="/" component={homeScreen} />
+            <Route exact path="/demo1" component={demoScreen1} />
+            <Route exact path="/demo2" component={demoScreen2} />
+            <Route exact path="/demo2a" component={demoScreen2a} />
+            <Route exact path="/demo3" component={demoScreen3} />
+            <Route exact path="/demo3a" component={demoScreen3a} />
+            <Route exact path="/demo4" component={demoScreen4} />
+            <Route exact path="/demo5" component={demoScreen5} />
             <Route path="/about" component={About} />
           </div>
         </MuiThemeProvider>
